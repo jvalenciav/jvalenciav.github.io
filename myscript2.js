464bed6d72b9f3480d72ac6ba811aca9bfb17504
jvalenciav <89419561+jvalenciav@users.noreply.github.com>
2022-11-07 10:37:33 -0600

    Add files via upload

---
+++ myscript2.js
@@ -0,0 +1,52 @@
+class Heart {
+    constructor(id) {
+      const heart = document.getElementById(id);
+      const text = heart.querySelector('.heart-text');
+      const sayings = [
+      'Eres Preciosa',
+      'Mi Fer :3',
+      'Mi Reyna',
+      'Me encantas',
+      'Nos casamos?',
+      'Te amo',
+      'Tu&Yo-Que rico',
+      'Bellaa',
+      'Princesaaa',
+      'Mi bebita',
+      'Mi ruca',
+      'Mi vieja',
+      'Me encantas',
+      'Orgulloso de ti',
+      'Eres miaa,ok?',
+      'Mi todo',
+      'Mi paz ',
+      'Mi Hogar'];
+  
+  
+      heart.addEventListener('click', () => {
+        this.addText(text, sayings);
+      });
+    }
+  
+    getText(sayings) {
+      const selection = parseInt(Math.random() * sayings.length);
+      return sayings[selection];
+    }
+  
+    addText(container, sayings) {
+      const str = this.getText(sayings);
+      const words = str.split(' ');
+  
+      container.innerHTML = '';
+  
+      words.forEach(word => {
+        const span = document.createElement('span');
+        span.innerHTML = word;
+        container.appendChild(span);
+      });
+  
+      return container;
+    }}
+  
+  
+  const heartContainer = new Heart('heart-container');
